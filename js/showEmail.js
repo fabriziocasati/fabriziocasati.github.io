325a19b682c23b53cfaacc64812c51c9f14bdaff
fabriziocasati <casati.fabrizio@gmail.com>
2016-12-23 20:53:55 +0100

    Update files

--- js/showEmail.js
+++ js/showEmail.js
@@ -1,4 +1,4 @@
 function showEmail() {
-    var myemail = "fabriziocasati.github@gmail.com"
+    var myemail = "fabrizio" + "casati" + ".github@gmail.com"
     alert("My email is:\n\n\t" + myemail + "\n\n[Note: you can select the email above with your mouse, so that it can be copied by pressing Ctrl+C and then pasted with Ctrl+V.]");
 }
