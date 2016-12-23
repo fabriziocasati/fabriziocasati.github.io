c78d0c0d5d06c727646bda650cc2eaffd7f771db
fabriziocasati <casati.fabrizio@gmail.com>
2016-12-23 22:24:57 +0100

    Update files

--- js/showEmail.js
+++ js/showEmail.js
@@ -3,6 +3,16 @@ function showEmail() {
     alert("My email is:\n\n\t" + myemail + "\n\n[Note: you can select the email above with your mouse, so that it can be copied by pressing Ctrl+C and then pasted with Ctrl+V.]");
 }
 
-function show() {
+function show2() {
     return "hello";
 }
+
+function show() {
+    return gen_mail_to_link('john.doe','example.com','Feedback about your site...');
+}
+
+function gen_mail_to_link(lhs,rhs,subject) {
+        document.write("<a href=\"mailto");
+        document.write(":" + lhs + "@");
+        document.write(rhs + "?subject=" + subject + "\">" + lhs + "@" + rhs + "<\/a>");
+    }
