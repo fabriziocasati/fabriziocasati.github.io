dbd7eff1b51aa3aa4e9353a62ccdfe3a64067d8f
fabriziocasati <casati.fabrizio@gmail.com>
2016-12-23 23:14:38 +0100

    Update files

--- js/contact.js
+++ js/contact.js
@@ -20,8 +20,8 @@ function gen_mail_to_link(lhs,rhs,subject) {
 }
 
 function generateEmail(a1,a2,a3,a4,a5) {
-    var x = a1 + a2 + "." + a3 + "@" + a4 + "." + a5
-    return "<a href=\"" + "mail" + "to" + ":" + a1 + a2 + "." + a3 + "@" + a4 + "." + a5 + "\">" + x + "<\/a>";
+    var email = a1 + a2 + "." + a3 + "@" + a4 + "." + a5;
+    return "<a href=\"" + "mail" + "to" + ":" + email + "\">" + email + "<\/a>";
 }
 
 function generateEmail2(lhs,rhs,subject) {
