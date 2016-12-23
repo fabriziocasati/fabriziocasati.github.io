8657d7ab3c38d66aea09f6b7947a977504852c51
fabriziocasati <casati.fabrizio@gmail.com>
2016-12-23 20:48:16 +0100

    Update files

--- 1.js
+++ 1.js
@@ -1,3 +1,4 @@
-function fn1 (){
-    alert("hello");
+function showEmail() {
+    var myemail = "fabriziocasati.github@gmail.com"
+    alert("My email is:\n\n\t" + myemail + "\n\n[Note: you can select the email above with your mouse, so that it can be copied by pressing Ctrl+C and then pasted with Ctrl+V.]");
 }
