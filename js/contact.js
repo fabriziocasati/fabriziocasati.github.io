2cd71e04682b835e48866371b58ffd855db4620c
fabriziocasati <casati.fabrizio@gmail.com>
2017-06-10 21:57:10 +0200

    Update pages

--- js/contact.js
+++ js/contact.js
@@ -1,3 +1,8 @@
+var xFoo = document.createElement('x-foo');
+xFoo.addEventListener('click', function(e) {
+  alert('Thanks!');
+});
+
 function show() {
     var myemail = "fabrizio" + "casati" + ".github@gmail.com";
     alert("My email is:\n\n\t" + myemail + "\n\n[Note: you can select the email above with your mouse, so that it can be copied by pressing Ctrl+C and then pasted with Ctrl+V.]");
